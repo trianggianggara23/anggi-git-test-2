fa450da08a43f379e2dde7ec9c0eab516ed9d2d2
trianggianggara23 <trianggianggara23@gmail.com>
2021-02-06 19:48:29 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci suscipit cum laborum aspernatur ad nesciunt deserunt. Ipsa ut doloremque vero deserunt veritatis dolorem maxime incidunt! Ipsa distinctio harum explicabo nihil?</p>
+
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>
 </body>
 </html>
