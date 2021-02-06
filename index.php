b251d295f7704eda93a8a82d169c8af02c77363e
trianggianggara23 <trianggianggara23@gmail.com>
2021-02-06 19:42:14 +0700

    Menambahkan title, h1, dan paragraf

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Coba Git Remote</title>
 </head>
 <body>
-    
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci suscipit cum laborum aspernatur ad nesciunt deserunt. Ipsa ut doloremque vero deserunt veritatis dolorem maxime incidunt! Ipsa distinctio harum explicabo nihil?</p>
 </body>
 </html>
